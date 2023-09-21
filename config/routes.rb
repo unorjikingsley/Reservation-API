feec530fd70c01c247842776c261f747232f3884
unorji kingsley <unorjikingsley@gmail.com>
2023-09-21 14:36:08 +0100

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  namespace :api do
-    namespace :v1 do
-      # get '#'
-    end
-  end
+
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :users, only: %i[index update show create destroy]
+  #     resources :reservations, only: %i[index show update create destroy]
+  #     resources :cars, only: %i[index update show create destroy]
+  #   end
+  # end
+
+  # devise_for :users, controllers: {
+  #   sessions: 'api/v1/sessions',
+  #   registrations: 'api/v1/registrations'
+  # }
 end
