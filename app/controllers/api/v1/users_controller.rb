8bfe0ff51c265f0353b65e81db7018be269ba529
Bifabig <biftubig@gmail.com>
2023-09-21 15:17:16 +0300

    add users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,43 +1,43 @@
 class Api::V1::UsersController < ApplicationController
-  # def index
-  #   @users = User.all
-  #   render json: @users
-  # end
+  def index
+    @users = User.all
+    render json: @users
+  end
 
-  # def show
-  #   @user = User.find(params[:id])
-  #   render json: @user
-  # end
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
 
-  # def create
-  #   @user = User.new(user_params)
+  def create
+    @user = User.new(user_params)
 
-  #   if @user.save
-  #     render json: @user, status: :created
-  #   else
-  #     render json: { error: 'Error creating user' }, status: :unprocessable_entity
-  #   end
-  # end
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { error: 'Error creating user' }, status: :unprocessable_entity
+    end
+  end
 
-  # def update
-  #   @user = User.find(params[:id])
+  def update
+    @user = User.find(params[:id])
 
-  #   if @user.update(user_params)
-  #     render json: @user
-  #   else
-  #     render json: { error: 'Error updating user' }, status: :unprocessable_entity
-  #   end
-  # end
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: { error: 'Error updating user' }, status: :unprocessable_entity
+    end
+  end
 
-  # def destroy
-  #   @user = User.find(params[:id])
-  #   @user.destroy
-  #   head :no_content
-  # end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    head :no_content
+  end
 
-  # private
+  private
 
-  # def user_params
-  #   params.require(:user).permit(:username, :email, :password)
-  # end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
