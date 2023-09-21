51bb52d2931f29118ffa9d6f6baee47f6d890fd3
unorji kingsley <unorjikingsley@gmail.com>
2023-09-21 02:10:36 +0100

    Added reservation relatiosnhips between tables

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
-  # belongs_to :user
-  # belongs_to :car
+  belongs_to :user
+  belongs_to :car
 
-  # validates :city, :start_date, :end_date, presence: true
+  validates :city, :start_date, :end_date, presence: true
 end
