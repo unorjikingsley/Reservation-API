76fca943c888a1f3e67f810260abbac8afb3bacd
unorji kingsley <unorjikingsley@gmail.com>
2023-09-21 18:55:32 +0100

    Modify routes to involved rswag

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[index update show create destroy]
-      resources :reservations, only: %i[index show update create destroy]
-      resources :cars, only: %i[index update show create destroy]
+    devise_for :users, controllers: {
+      sessions: 'api/v1/sessions',
+      registrations: 'api/v1/registrations'
+    }
+
+    resources :users, only: %i[index update show create destroy]
+    resources :reservations, only: %i[index show update create destroy]
+    resources :cars, only: %i[index update show create destroy]
     end
   end
-
-  devise_for :users, controllers: {
-    sessions: 'api/v1/sessions',
-    registrations: 'api/v1/registrations'
-  }
 end
