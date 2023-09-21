fa2d0a1557addb7705ca8b04ebaaaadf632d11f5
Bifabig <biftubig@gmail.com>
2023-09-21 16:45:34 +0300

    add users, reservations and cars routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :users, only: %i[index update show create destroy]
-  #     resources :reservations, only: %i[index show update create destroy]
-  #     resources :cars, only: %i[index update show create destroy]
-  #   end
-  # end
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index update show create destroy]
+      resources :reservations, only: %i[index show update create destroy]
+      resources :cars, only: %i[index update show create destroy]
+    end
+  end
 
-  # devise_for :users, controllers: {
-  #   sessions: 'api/v1/sessions',
-  #   registrations: 'api/v1/registrations'
-  # }
+  devise_for :users, controllers: {
+    sessions: 'api/v1/sessions',
+    registrations: 'api/v1/registrations'
+  }
 end
