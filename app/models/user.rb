4ab6765e7c3e109e7f15ff0336650c514b93714f
unorji kingsley <unorjikingsley@gmail.com>
2023-09-21 01:20:26 +0100

    Models and Contollers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  # has_many :reservations, foreign_key: 'user_id', dependent: :destroy
+  # has_many :cars, foreign_key: 'user_id', dependent: :destroy
+
+  # validates :username, :email, :password, presence: true
 end
