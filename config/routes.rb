c2f2157cbc2f57e75a69615a685997c395692669
Bifabig <biftubig@gmail.com>
2023-09-21 16:48:39 +0300

    remove unwanted commets

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   namespace :api do
     namespace :v1 do
