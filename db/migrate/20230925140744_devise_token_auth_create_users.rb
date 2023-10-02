98969c4d34d8991327363f6506ab4229f4de3f68
unorji kingsley <unorjikingsley@gmail.com>
2023-10-02 14:50:12 +0100

    removed double column error

--- db/migrate/20230925140744_devise_token_auth_create_users.rb
+++ db/migrate/20230925140744_devise_token_auth_create_users.rb
@@ -24,11 +24,11 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
       t.string   :unconfirmed_email # Only if using reconfirmable
 
       # Trackable
-      t.integer  :sign_in_count, default: 0, null: false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.inet :current_sign_in_ip
-      t.inet :last_sign_in_ip
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.inet :current_sign_in_ip
+      # t.inet :last_sign_in_ip
 
       ## Lockable
       # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
