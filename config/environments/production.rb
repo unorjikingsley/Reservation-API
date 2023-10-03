9789efb7ec242ee81152e3fd95e063638d1955e3
unorji kingsley <unorjikingsley@gmail.com>
2023-10-03 10:55:28 +0100

    Change mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://carhub-nxj0.onrender.com',  protocol: "https" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
